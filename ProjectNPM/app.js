47f3625f6ab9d52096a6ee557a6936006c0f07af
ArpitVerma3 <varpit520@gmail.com>
2026-07-16 23:54:18 +0530

    middleware

--- ProjectNPM/app.js
+++ ProjectNPM/app.js
@@ -3,11 +3,20 @@ const express = require("express");
 const http = require("http");
 
 const app = express();
-const server = http.createServer(app);
 
-const requestHandler = http.createServer(userReqHandler);
+app.use((req, res, next) => {
+  console.log("Came in first middlewear", req.url, req.method);
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Came in second middlewear", req.url, req.method);
+  next();
+});
+
+const server = http.createServer(app);
 
 const PORT = 3002;
 server.listen(PORT, () => {
-  console.log(`server running on adrs http://localhost:$(PORT)`);
+  console.log(`server running on adrs http://localhost:${PORT}`);
 });
